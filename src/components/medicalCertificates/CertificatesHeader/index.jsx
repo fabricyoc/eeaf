cefcb7ddea5605b4f62757846613cb049a1f7403
fabricyoc <fazin_c@hotmail.com>
2026-07-22 15:15:01 -0300

    fix: medicalCertificates

--- src/components/medicalCertificates/CertificatesHeader/index.jsx
+++ src/components/medicalCertificates/CertificatesHeader/index.jsx
@@ -1,10 +1,7 @@
 import { FaPlus } from "react-icons/fa";
-
 import HeaderDashboard from "../../HeaderDashboard";
 import SearchBar from "../../ui/SearchBar";
-
 import styles from "./CertificatesHeader.module.css";
-
 
 const SEARCH_FIELDS = [
   "alunos.nome",
@@ -14,77 +11,41 @@ const SEARCH_FIELDS = [
   "data_fim",
 ];
 
-
 function CertificatesHeader({
-
   atestados,
-
   onSearch,
-
   setBuscaAtiva,
-
   onNovo,
-
   canCreateCertificates,
-
 }) {
-
 
   return (
 
     <HeaderDashboard
       title="Atestados dos Alunos"
     >
-
-
       <div className={styles.actions}>
-
-
         <SearchBar
-
           items={atestados}
-
           onSearch={onSearch}
-
           setBuscaAtiva={setBuscaAtiva}
-
           fields={SEARCH_FIELDS}
-
           placeholder="Pesquise aluno(a), turma ou data..."
-
         />
-
-
-
         {
           canCreateCertificates && (
-
             <button
-
               className={styles.button}
-
               onClick={onNovo}
-
             >
-
               <FaPlus />
-
               Novo Atestado
-
             </button>
-
           )
         }
-
-
       </div>
-
-
     </HeaderDashboard>
-
   );
-
 }
-
 
 export default CertificatesHeader;
